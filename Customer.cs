90f896e98943046bd7b0760e477cdf54bbca5d02
Paul Cortez <cortezpaul19@gmail.com>
2020-11-07 18:21:52 -0600

    Added New Class Transaction and  Customer. Modified Main Class

---
+++ Customer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace StoreApplication
+{
+	/// <summary>
+	/// Summary description for Class1
+	/// </summary>
+	public class Customer
+	{
+		//Fields
+        public string ID { get; }
+
+		public string Name { get; set; }
+
+		public decimal Balance 
+		{ 
+			get
+            {
+				decimal balance = 0;
+				foreach (var item in allTransactions)
+                {
+					balance += item.Price; 
+                }
+				return balance;
+            }
+		
+		}
+
+		private static int customersID = 1;
+
+		public List<Transaction> allTransactions = new List<Transaction>();
+
+		
+		//Constructor
+		public Customer(string name)
+		{
+		
+			this.Name = name;
+			this.ID = customersID.ToString();
+			customersID++;
+		}
+
+		//Methods
+		public void AddItem(decimal price, string itemName, string itemID, DateTime date)
+        {
+			if (price <= 0 )
+            {
+				throw new ArgumentOutOfRangeException(nameof(price), "Price of an Item must not be negative!");
+            }
+			var buy = new Transaction(price, itemName, itemID, date);
+			allTransactions.Add(buy);
+        }
+
+		public void RemoveItem(decimal price, string itemName, string itemID, DateTime date)
+        {
+			if (price <= 0)
+            {
+				throw new ArgumentOutOfRangeException(nameof(price), "Cannot Remove Item with an empty Cart!");
+            }
+			if (Balance - price < 0)
+            {
+				Console.WriteLine("Cart is Empty!");
+            }
+			var remove = new Transaction(-price, itemName, itemID, date);
+			allTransactions.Add(remove);
+        }
+	}
+}
---
+++ MainDisplay.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Xml;
+
+namespace StoreApplication
+{
+    class MainDisplay
+    {
+         static void Main(string[] args)
+        {
+            string choice;
+            bool shopMore = true;
+            Console.WriteLine("\n\nProject 0: Store Application ");
+           
+            do
+            {   
+                Console.Write("\nSelect the right Option!:");
+                Console.WriteLine("\n1. Customer's Options" + 
+                             "\n2. Store's Options" +
+                             "\n3. End Program");
+                             
+                choice = Console.ReadLine(); //Throw Exception
+                switch (choice)
+                {
+                    case "1": 
+                        string customerChoice;
+                        Console.WriteLine("\n\nCustomer's Options" +
+                                        "\n1.Register" +
+                                        "\n2.Shop" +
+                                        "\n3.Show Order" +
+                                        "\n(Press any key to Exit)");
+                        Console.Write("\nSelect the right option you want:");
+
+                        //Throw Exception
+                        customerChoice = Console.ReadLine();
+
+                            if (customerChoice == "1")
+                            {
+                                var account = new Customer(Console.ReadLine());
+                                Console.WriteLine($"Account number{account.ID} was created for {account.Name} with {account.Balance} Balance");
+
+                            }
+                        
+                                else if (customerChoice == "2")
+                            {
+                                //Shop Here
+                            }
+                        
+                                else if (customerChoice == "3")
+                            {
+                                //Show order
+                            }
+
+                                else
+                            {
+                                //Terminate Program
+                            }
+
+                        break;
+
+                    case "2": //throw exception for wrong input
+                        Console.WriteLine("\n\nOwner's Options" +
+                                        "\n1.Add Store" +
+                                        "\n2.Show Order History" +
+                                        "\n3.Show Products" +
+                                        "\n4.Add Products"+
+                                        "\n(Press any key to Exit)");
+                        string ownerChoice;
+                        Console.Write("\nSelect the right option you want:");
+                        
+                        //Throw Exception
+                        ownerChoice = Console.ReadLine();
+
+                        if (ownerChoice == "1")
+                            {
+                                //add store
+                            }
+                        
+                                else if (ownerChoice == "2")
+                            {
+                                //show order history
+                            }
+                        
+                                else if (ownerChoice == "3")
+                            {
+                                //Show product
+                            }
+                                else if (ownerChoice == "4")
+                            {
+                                //add product
+                            }
+
+                                else
+                            {
+                                //Terminate Program
+                            }
+                
+                        break;
+
+                    case "3":
+                            Console.WriteLine("GoodBye Shopper!");
+                            shopMore = false;
+                        break;
+                            //force terminate here
+
+                }
+            
+            //throw exception for wrong input
+            Console.WriteLine("\n\nDo you want to shop more? Press \"1\" for Yes or \"0\" For No");
+            string pick;
+            pick = Console.ReadLine();
+            if (pick == "1")
+            {
+                shopMore = true;
+            }
+            else
+            {
+                shopMore = false;
+            }
+
+            } while (shopMore);
+        }
+    }
+}
